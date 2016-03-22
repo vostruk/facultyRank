4e689b608e8011be522310f0e09af30bde740cd8
vovkaOst <vovaostruki@gmail.com>
2016-03-22 22:38:55 +0100

    managing admin panel to display only relevant properties

--- university/admin.py
+++ university/admin.py
@@ -1,14 +1,51 @@
 from django.contrib import admin
 import sys
 from .models import University, Faculty, Indicator, FacultyIndicators
+from django.utils.translation import ugettext_lazy as _
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 class FacultyAdmin(admin.ModelAdmin):
     fields = ['name', 'shortcut', 'university']
+    list_display = ('name', 'shortcut', 'university')
+
+
+class RelatedFacultyFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('Related faculty')
+
+    parameter_name = 'faculty'
+
+    def lookups(self, request, model_admin):
+        l = []
+        for g in request.user.groups.all():
+            param3 = g.name.split("$")
+            if len(param3)>1:
+                f_name = param3[0].split('-')[0]
+                l.append((param3[1], f_name))
+        return l
+
+    def queryset(self, request, queryset):
+        if self.value():
+  #          if request.user.is_superuser:
+   #             return queryset
+            return queryset.filter(faculty__id__exact=self.value())
+      #  else:
+     #       return queryset
+
+
+class FacultyIndicatorsAdmin(admin.ModelAdmin):
+    fields = ['value', 'pub_date']
+    list_display = ('indicator', 'faculty', 'value', 'pub_date')
+    list_filter = (RelatedFacultyFilter,)
+    search_fields = ['indicator__name']
+    list_per_page = 20
+
 
 admin.site.register(University)
 admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Indicator)
-admin.site.register(FacultyIndicators)
+admin.site.register(FacultyIndicators,FacultyIndicatorsAdmin)
--- university/models.py
+++ university/models.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 from django.db.models import signals
 from django.db import models
-from django.dispatch import dispatcher
 import datetime
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
+
 
 class University(models.Model):
     name = models.CharField(max_length=200)
@@ -40,8 +41,11 @@ class FacultyIndicators(models.Model):
     value = models.FloatField(default=0)
     pub_date = models.DateTimeField('date published')
 
+    def full_name(self):
+        return self.indicator.name
+
     def __str__(self):
-        return self.faculty.shortcut + ":" + self.indicator.shortcut + "=" + str(self.value)
+        return self.faculty.shortcut + " : " + self.indicator.shortcut
 
 
 # sender - The model class. (MyModel)
@@ -49,12 +53,26 @@ class FacultyIndicators(models.Model):
 # created - Boolean; True if a new record was created.
 # *args, **kwargs - Capture the unneeded `raw` and `using`(1.3) arguments.
 @receiver(signals.post_save,sender=Faculty)
-def my_model_post_save(sender, instance, created, *args, **kwargs):
-    print("in model save signal")
+def faculty_post_save(sender, instance, created, *args, **kwargs):
     if created:
-        print("in model created")
+        print("Trigger for indicators after new faculty was created")
+        #create a group with a faculty name to be used for faculty staff
+        new_group, group_created = Group.objects.get_or_create(name=instance.name + '-' +
+                                                                    instance.university.name+"$"+str(instance.id))
+        if group_created:
+            new_group.save()
+        #trigger a list of indicators for this faculty
         indicators = Indicator.objects.all()
         for i in indicators:
-            new_relation = FacultyIndicators(sender, i, 0, datetime.datetime.now())
+            new_relation = FacultyIndicators.objects.create(faculty_id=instance.id, indicator_id=i.id, value = 0.0, pub_date = datetime.datetime.now())
+            new_relation.save()
+
+@receiver(signals.post_save,sender=Indicator)
+def indicator_post_save(sender, instance, created, *args, **kwargs):
+    if created:
+        print("Trigger for faculties after new indicator was created")
+        faculties = Faculty.objects.all()
+        for f in faculties:
+            new_relation = FacultyIndicators.objects.create(faculty_id=f.id, indicator_id=instance.id, value = 0.0, pub_date = datetime.datetime.now())
             new_relation.save()
 
